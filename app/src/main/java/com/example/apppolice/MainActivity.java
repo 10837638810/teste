8b6803433f514bce27de51bf81bf2ad39acfc654
10bpmmtelematica <41bpmm.04.telematica@gmail.com>
2021-02-27 14:56:50 -0300

    vs. 1.0.1

--- app/src/main/java/com/example/apppolice/MainActivity.java
+++ app/src/main/java/com/example/apppolice/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.apppolice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+        editText = findViewById(R.id.editText3);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = editText.getText().toString();
+                if(str.equals("123456789")){
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "CPF não cadastrado",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }
